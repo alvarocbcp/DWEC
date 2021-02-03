abb96351469e18dd17938a2cc7f0cfe80d2cc14f
alvarocbcp <alvarocbcp@gmail.com>
2021-02-03 20:46:00 +0100

    actualizacion form

--- MIGUEL/Form/app.js
+++ MIGUEL/Form/app.js
@@ -1,57 +1,64 @@
- //Validacion de formularios (simple)
+//Validacion de formulario (simple)
 
- //Primero hay que seleccionar el formulario
+//Primero hay que seleccionar formulario
 
- //Conociendo ID
+//Conociendo ID
 var formulario1 = document.getElementById("miFormulario");
-var formuario2 = document.forms("miFormulario");
+var fomulario2 = document.forms["miFormulario"];
 
 //Por el numero de formulario
 var formulario3 = document.getElementsByTagName("form"[0]);
 var formulario4 = document.forms[0];
 
-/*
-Seleccionar elementos del formulario
+/* Seleccionar elementos del formulario
 .elements[] Devuelve un array con todos los inputs del formulario
-getElementById("Elemento") Devuelve un elemento con un ID determinado
-getElementByTagName("etiqueta") Devuelve un array con elementos del tipo etiqueta
-getElementByName("nombre") Devuelve un array con los elementos del mismo nombre
+getElementbyID("idElemento") Devuelve un elemento con un id determinado
+getElementbyTagName("etiqueta") Devuelve un array con elemento del tipo etiqueta
+getElementbyName("nombre") Devuelve un array con los elementos del mismo nombre
 */
 
 window.onload = iniciar;
 
 function iniciar(){
-    document.getElementById("enviar").addEventListener('click', validar, false);
+    document.getElementById("enviar").addEventListener('click', validar,false);
 }
 
 function validarNombre(){
     var elemento = document.getElementById("nombre");
     if(elemento.value == ""){
-        alert("El campo no puede estar vacio");
+        alert("El campo nombre no puede quedar vacio");
+        error(elemento);
         return false;
     }
-    return true;
+    else{
+        return true;
+    }
 }
 
 function validarTelefono(){
-    var elememto = document.getElementById("telefono");
-    if(isNaN(elemento.value)){
-        alert("El campo telefono debe ser numerico");
+    var elemento = document.getElementById("telefono").value;
+    if(isNaN(elemento) || elemento==""){
+        alert("El campo telefono debe ser un numero valido")
         return false;
     }
-    return true;
+    else{
+        return true;
+    }
 }
 
 function validarFecha(){
     var dia = document.getElementById("dia").value;
     var mes = document.getElementById("mes").value;
     var anio = document.getElementById("anio").value;
-    var fecha = new Date(anio, mes, dia);
-    if(isNaN(fecha)){
-        alert("Los campos de la fecha son incorrectos");
+
+    var fecha = new Date(anio,mes,dia);
+    if(isNaN(fecha) || dia=="" || mes=="" || anio==""){
+        alert("Los campos de la fecha son incorrectos")
         return false;
     }
-    return true;
+    else{
+        return true;
+    }
 }
 
 function validarCheck(){
@@ -60,15 +67,27 @@ function validarCheck(){
         alert("Debes ser mayor de edad");
         return false;
     }
-    return true;
+    else{
+        return true;
+    }
 }
 
 function validar(e){
-    if(validarNombre() && validarTelefono() && validarFecha() && validarCheck() && confirm("Pulse aceptar si desea enviar el formulario")){
-        return true;
+    if(validarNombre() && validarTelefono() && validarFecha() && validarCheck() 
+        && confirm("Pulse aceptar si desea enviar el formulario")){
+            return true;
     }
     else{
         e.preventDefault();
         return false;
     }
+}
+
+function error(elemento){
+    elemento.className = "error";
+    elemento.focus();
+}
+
+function limpiarError(elemento){
+    elemento.className = "";
 }
