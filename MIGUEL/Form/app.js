3d3a72fcdf3552cc24930499c37413fe008cff01
alvarocbcp <alvarocbcp@gmail.com>
2021-02-08 18:57:28 +0100

    formulario con validacion

--- MIGUEL/Form/app.js
+++ MIGUEL/Form/app.js
@@ -1,81 +1,62 @@
-//Validacion de formulario (simple)
-
-//Primero hay que seleccionar formulario
-
-//Conociendo ID
-var formulario1 = document.getElementById("miFormulario");
-var fomulario2 = document.forms["miFormulario"];
-
-//Por el numero de formulario
-var formulario3 = document.getElementsByTagName("form"[0]);
-var formulario4 = document.forms[0];
-
-/* Seleccionar elementos del formulario
-.elements[] Devuelve un array con todos los inputs del formulario
-getElementbyID("idElemento") Devuelve un elemento con un id determinado
-getElementbyTagName("etiqueta") Devuelve un array con elemento del tipo etiqueta
-getElementbyName("nombre") Devuelve un array con los elementos del mismo nombre
-*/
-
 window.onload = iniciar;
 
 function iniciar(){
-    document.getElementById("enviar").addEventListener('click', validar,false);
+    document.getElementById("enviar").addEventListener('click', validar, false);
 }
 
-function validarNombre(){
+function validaNombre(){
     var elemento = document.getElementById("nombre");
-    if(elemento.value == ""){
-        alert("El campo nombre no puede quedar vacio");
-        error(elemento);
+    if(!elemento.checkValidity()){
+        if(elemento.validity.valueMissing){
+            error2(elemento, "Debe introducir un nombre");
+        }
+
+        if(elemento.validity.patternMismatch){
+            error2(elemento, "El nombre debe tener entre 2 y 15 caracteres");
+        }
+        //error(elemento);
         return false;
     }
-    else{
-        return true;
-    }
+    return true;
 }
 
-function validarTelefono(){
-    var elemento = document.getElementById("telefono").value;
-    if(isNaN(elemento) || elemento==""){
-        alert("El campo telefono debe ser un numero valido")
+function validaEdad(){
+    var elemento = document.getElementById("edad");
+    if(!elemento.checkValidity()){
+        if(elemento.validity.valueMissing){
+            error2(elemento, "Debe introducir su edad");
+        }
+        if(elemento.validity.rangeOverflow){
+            error2(elemento, "Su edad debe ser menor que 100 años");
+        }
+        if(elemento.validity.rangeUnderflow){
+            error2(elemento, "Su edad debe ser mayor que 18 años");
+        }
+        //error(elemento);
         return false;
     }
-    else{
-        return true;
-    }
+    return true;
 }
 
-function validarFecha(){
-    var dia = document.getElementById("dia").value;
-    var mes = document.getElementById("mes").value;
-    var anio = document.getElementById("anio").value;
-
-    var fecha = new Date(anio,mes,dia);
-    if(isNaN(fecha) || dia=="" || mes=="" || anio==""){
-        alert("Los campos de la fecha son incorrectos")
+function validaTelefono(){
+    var elemento = document.getElementById("telefono");
+    if(!elemento.checkValidity()){
+        if(elemento.validity.valueMissing){
+            error2(elemento, "Debe introducir un teléfono");
+        }
+        if(elemento.validity.patternMismatch){
+            error2(elemento, "El telefono debe tener 9 cifras")
+        }
+        //error(elemento);
         return false;
     }
-    else{
-        return true;
-    }
-}
-
-function validarCheck(){
-    var campoCheck = document.getElementById("mayor");
-    if(!campoCheck.checked){
-        alert("Debes ser mayor de edad");
-        return false;
-    }
-    else{
-        return true;
-    }
+    return true;
 }
 
 function validar(e){
-    if(validarNombre() && validarTelefono() && validarFecha() && validarCheck() 
-        && confirm("Pulse aceptar si desea enviar el formulario")){
-            return true;
+    borrarError();
+    if(validaNombre() && validaEdad() && validaTelefono() && confirm("Pulse aceptar si quiere enviar el formulario")){
+        return true;
     }
     else{
         e.preventDefault();
@@ -84,10 +65,20 @@ function validar(e){
 }
 
 function error(elemento){
+    document.getElementById("mensajeError").innerHTML = elemento.validationMessage;
+    elemento.className = "error";
+    elemento.focus();//Se pone el cursor en el input
+}
+
+function error2(elemento, mensaje){
+    document.getElementById("mensajeError").innerHTML = mensaje;
     elemento.className = "error";
     elemento.focus();
 }
 
-function limpiarError(elemento){
-    elemento.className = "";
+function borrarError(){
+    var formulario = document.forms["miFormulario"];
+    for(var i=0; i<formulario.length; i++){
+        formulario.elements[i].className = "";
+    }
 }
